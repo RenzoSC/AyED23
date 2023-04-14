0dad42239aecd09269f05e89fd3f5b5ee4d6cda5
RenzoSC <renzoc009@gmail.com>
2023-04-14 16:49:20 -0300

    finished ej3

--- lab03/ej3/main.c
+++ lab03/ej3/main.c
@@ -12,17 +12,40 @@ static void dump(char a[], unsigned int length) {
     printf("\n\n");
 }
 
-int main(/* -- completar-- */) {
+int main(int argc, char * argv[]) {
     FILE *file;
     unsigned int indexes[MAX_SIZE];
     char letters[MAX_SIZE];
     char sorted[MAX_SIZE];
-    unsigned int length=0; 
+    unsigned int length=0;
     //  .----------^
     //  :
     // Debe guardarse aqui la cantidad de elementos leidos del archivo
     
     /* -- completar -- */
+    unsigned int index;
+    
+    if (argc != 2)
+    {
+        printf("EXPECTED UN FILE AS ARGUMENT \nCORRECT USE: ./ejecutable ./file.in\n");
+        exit(EXIT_FAILURE);
+    }
+     
+    file = fopen(argv[1], "r");
+    while (!feof(file) && length <MAX_SIZE)
+    {
+        fscanf(file, "%u -> *%c* ", &index, &letters[length]);
+        if (index >= MAX_SIZE){
+            printf("INDEX MÁXIMO ALCANZADO, INVALID FILE\n");
+            exit(EXIT_FAILURE);}
+        indexes[length] = index;
+        length++;
+    }
+    fclose(file);
+    for (unsigned int i = 0; i < length; i++)
+    {
+        sorted[indexes[i]] = letters[i];
+    }
 
     dump(sorted, length);
 
